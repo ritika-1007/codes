377d7b798794222693c509bebbc0ac84339bb2c9
Ritika Upreti <ritikaupreti78@gmail.com>
2022-01-19 22:35:32 +0530

    19th Jan

---
+++ Documents/CODES/majele.java
@@ -0,0 +1,18 @@
+class majele {
+    public int majorityElement(int[] nums) {
+       int count=1,maj=nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(maj==nums[i])
+                count++;
+            else 
+                count--;
+            if(count==0)
+            {
+                count=1;
+                maj=nums[i];
+            }
+        }
+        return maj;
+    }
+}
